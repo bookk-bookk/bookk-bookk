8787bc434917713f1ddfc22833a284c1a9f04e45
kde6260 <kde6260@gmail.com>
2020-06-28 02:06:37 +0900

    ftr#6 nested dict를 dataclass로 리팩토링

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import json
+from dataclasses import asdict
 from http import HTTPStatus
 from typing import Optional
 
@@ -10,6 +11,7 @@ from slack.web.slack_response import SlackResponse
 from starlette.datastructures import FormData
 
 from helper import post_book_to_notion
+from model import dialog_format
 
 app = FastAPI()
 
@@ -17,46 +19,6 @@ slack_token: Optional[str] = os.environ.get("SLACK_API_TOKEN")
 slack_client = WebClient(token=slack_token, run_async=True)
 
 DIALOG_SUBMIT_DONE: str = "dialog_submission"
-
-# fmt: off
-DIALOG_FORMAT: dict = {
-    "callback_id": "test-test-callback",
-    "title": "책을 공유해주세요.",
-    "submit_label": "submit",
-    "notify_on_cancel": True,
-    "elements": [
-        {"label": "책 이름", "name": "book_name", "type": "text", },
-        {
-            "label": "카테고리",
-            "name": "category",
-            "type": "select",
-            "option_groups": [
-                {
-                    "label": "경영/경제",
-                    "options": [
-                        {"label": "경영일반", "value": "business-common", },
-                        {"label": "경제일반", "value": "economics-common", },
-                        {"label": "마케팅세일즈", "value": "marketing-sales", },
-                    ],
-                },
-                {
-                    "label": "인문/사회/역사",
-                    "options": [
-                        {"label": "인문", "value": "humanities", },
-                        {"label": "정치/사회", "value": "politics-society", },
-                        {"label": "예술/문화", "value": "art-culture", },
-                    ],
-                },
-            ],
-        },
-        {"label": "도서 링크", "name": "link", "type": "text", "subtype": "url", },
-        {"label": "추천 이유", "name": "recommend_reason", "type": "textarea", },
-        {"label": "출판사", "name": "publisher", "type": "text", },
-        {"label": "저자", "name": "author", "type": "text", },
-    ],
-}
-# fmt: on
-
 SUCCESS_MESSAGE: str = """
 📖 {username}님의 추천도서를 공유했습니다 📖
 {book_name} ({category}, {publisher} 출판, {author} 저)
@@ -69,7 +31,7 @@ SUCCESS_MESSAGE: str = """
 async def open_form(request: Request) -> Response:
     form_data: FormData = await request.form()
     response: SlackResponse = await slack_client.dialog_open(  # type: ignore
-        dialog=DIALOG_FORMAT, trigger_id=form_data.get("trigger_id"),
+        dialog=asdict(dialog_format), trigger_id=form_data.get("trigger_id"),
     )
     if response["ok"]:
         return Response()
---
+++ model.py
@@ -0,0 +1,61 @@
+import enum
+import uuid
+from dataclasses import dataclass
+from typing import List, Optional
+
+
+@dataclass(repr=True)
+class DialogOption:
+    label: str
+    value: str
+
+
+@dataclass(repr=True)
+class DialogOptionGroup:
+    label: str
+    options: List[DialogOption]
+
+
+@dataclass(repr=True)
+class DialogElement:
+    label: str
+    name: str
+    type: str
+    option_groups: Optional[List[DialogOptionGroup]] = None
+    subtype: Optional[str] = None
+
+
+@dataclass(repr=True)
+class DialogFormat:
+    title: str
+    elements: List[DialogElement]
+    submit_label: str = "submit"
+    callback_id: str = uuid.uuid4().hex
+    notify_on_cancel: bool = True
+
+
+class BookCategories(enum.Enum):
+    ECONOMIC_BUSINESS = "경영/경제", ("경영일반", "경제일반", "마케팅세일즈")
+    HUMANITY_SOCIETY_HISTORY = "인문/사회/역사", ("인문", "정치/사회", "예술/문화")
+
+    @classmethod
+    def get_option_groups(cls):
+        options_groups = []
+        for category in cls:
+            main, subs = category.value
+            options = [DialogOption(label=sub, value=sub) for sub in subs]
+            option_group = DialogOptionGroup(label=main, options=options)
+            options_groups.append(option_group)
+        return options_groups
+
+
+dialog_format = DialogFormat(
+    title="책을 공유해주세요.",
+    elements=[
+        DialogElement(label="책 이름", name="book_name", type="text"),
+        DialogElement(label="카테고리", name="category", type="select", option_groups=BookCategories.get_option_groups()),
+        DialogElement(label="도서 링크", name="link", type="text", subtype="url"),
+        DialogElement(label="출판사", name="publisher", type="text"),
+        DialogElement(label="저자", name="author", type="text"),
+    ],
+)
