1ad5c759e2a2ac6a0efa65aa587d110b3701bfca
dev-daeun <devdaeun@gmail.com>
2023-04-09 04:21:26 +0900

    This reverts commit 9a08a489508c7df12dbe952f4d2154f2e14f56d6.

--- app.py
+++ app.py
@@ -87,6 +87,6 @@ async def submit_book(request: Request) -> Response:
         return Response(content=post_message_res.error)
 
     # posting to notion is intended to run background.
-    post_book_to_notion(book)
+    event_loop.call_later(0, post_book_to_notion, book)
 
     return Response()
