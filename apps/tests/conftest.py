b77926d9f7d6a1a11c17befd672deaadb608132b
dev-daeun <devdaeun@gmail.com>
2025-02-24 16:42:53 +0900

    #74 conftest.py 파일 수정

--- apps/tests/conftest.py
+++ apps/tests/conftest.py
@@ -1,1 +1,6 @@
-pytest_plugins = ["tests.fixtures"]
+"""
+https://docs.pytest.org/en/stable/how-to/writing_plugins.html
+Requiring plugins using pytest_plugins variable in non-root conftest.py files is deprecated.
+-> import 하는 것으로 대체
+"""
+from tests.fixtures import *  # noqa: F401,F403
